d34339f0ee36bc138e2e11d56b37ef56e30aff13
erickalves <erick.pontes@aluno.ifsp.edu.br>
2025-06-11 14:47:14 -0300

    deletando eventos

---
+++ resources/views/events/dashboard.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.main')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+<div class="col-md-10 offset-md-1 dashboard-title-container">
+    <h1>Meus eventos</h1>
+</div>
+
+<div class="col-md-10 offset-md-1 dashboard-title-container">
+    @if(count($events)>0)
+    <table class="table">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Participantes</th>
+            <th scope="col">Ações</th>
+        </tr>
+    
+    <tbody>
+        @foreach($events as $event)
+        <tr>
+            <td scropt="row">{{$loop->index + 1}}</td>
+            <td><a href="/events/{{$event->id}}">{{$event->title}}</a></td>
+            <td>0</td>
+            <td>
+                <a href="#" class="btn btn-info edit-btn">Editar</a> 
+                <form action="/events/{{$event->id}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger delete-btn">Deletar</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+    </table>
+    @else
+    <p>Você ainda não tem eventos. <a href="/events/create">Criar eventos</a></p>
+    @endif
+</div>
+@endsection
